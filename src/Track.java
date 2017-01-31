1d2f35819079b5b2fae9ca8f17294e670e8c2bee
Lane Lutgen <lutgenlane@gmail.com>
2017-01-30 17:33:56 -0700

    Fixed issue with pin and cvv. Added regex for track 2.

--- src/Track.java
+++ src/Track.java
@@ -15,39 +15,51 @@ public class Track {
         String pin = null;
 	String s = null;
 	BufferedReader buffer = null;
-        Matcher match = null;
+        Matcher matchOne = null;
+        Matcher matchTwo = null;
 
-        String pattern = "(?:4[0-9]{15}|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\\d{3})\\d{11})(?:.[a-zA-Z]+.[a-zA-Z]+.)[0-9][0-9](?:0[1-9]|1[0-2])\\d{3}";
+        String patternOne = "(?:4[0-9]{15}|5[1-5][0-9]{14}|3[47][0-9]{13}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\\d{3})\\d{11})(?:.[a-zA-Z]+.[a-zA-Z]+.)[0-9][0-9](?:0[1-9]|1[0-2])\\d{3}";
+        String patternTwo = ";\\d{0,19}=\\d{7}\\w*\\?";
         
-        Pattern r = Pattern.compile(pattern);
+        Pattern trackOnePattern = Pattern.compile(patternOne);
+        Pattern trackTwoPattern = Pattern.compile(patternTwo);
+        
         int i = 0;
         
         try {
-		buffer = new BufferedReader(new FileReader("/Users/Fenil/Desktop/CSCI 476/Labs/Lab 1/memorydump.dmp"));
+		buffer = new BufferedReader(new FileReader("input/memorydump.dmp"));
 		while ((s = buffer.readLine()) != null) {
-			match = r.matcher(s);
-	     		if (match.find( )) {
-				i++;
+			matchOne = trackOnePattern.matcher(s);
+			matchTwo = trackTwoPattern.matcher(s);
+	     		if (matchOne.find( )) {
+					i++;
+						
+		     			String str = matchOne.group(0);
+					str = str.replace('/','\0');
+		     			str = str.replace('^','\0');
+						
+					cnum = str.substring(0,16);
+		     			cname = str.substring(16,str.length()-8);
+					year = str.substring(str.length()-7,str.length()-5);
+		     			month = str.substring(str.length()-5,str.length()-3);
 					
-	     			String str = match.group(0);
-				str = str.replace('/','\0');
-	     			str = str.replace('^','\0');
+				     		
+					System.out.println("<Information of the "+ i +"st credit card>");
+		     			System.out.println("Cardholder's Name: " +cname);
+					System.out.println("Card Number: " +cnum);
+	  	         		System.out.println("Expiration Date: " +month+ "/20" +year);
 					
-				cnum = str.substring(0,16);
-	     			cname = str.substring(16,str.length()-8);
-				year = str.substring(str.length()-7,str.length()-5);
-	     			month = str.substring(str.length()-5,str.length()-3);
-			/*	pin = str.substring(str.length()-7,str.length()-3);*/
-	     			cvv = str.substring(str.length()-3,str.length());
-			     		
-				System.out.println("<Information of the "+ i +"st credit card>");
-	     			System.out.println("Cardholder's Name: " +cname);
-				System.out.println("Card Number: " +cnum);
-  	         		System.out.println("Expiration Date: " +month+ "/20" +year);
-				System.out.println("Encrypted PIN: " +pin);
+	     		}
+	     		if(matchTwo.find()){
+	     			
+	     			String str = matchTwo.group(0);
+	     			pin = str.substring(str.length()-10,str.length()-6);
+	     			cvv = str.substring(str.length()-6,str.length()-3);
+	     			
+	     			System.out.println("Encrypted PIN: " +pin);
   	         		System.out.println("CVV Number: " +cvv);
-			}
-		}   
+	     		}
+			}   
         }
 		
 	catch (IOException e) {
