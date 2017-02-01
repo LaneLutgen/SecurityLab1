bab2b32be1278cb4a30707e3922f354ca9d3575b
Fenil Patel <fenil_patel93@ymail.com>
2017-01-31 17:37:33 -0700

    Changed Track 1 regular expression

--- src/Track.java
+++ src/Track.java
@@ -18,8 +18,8 @@ public class Track {
         Matcher matchOne = null;
         Matcher matchTwo = null;
 
-        String patternOne = "(?:4[0-9]{15}|5[1-5][0-9]{14}|3[47][0-9]{13}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\\d{3})\\d{11})(?:.[a-zA-Z]+.[a-zA-Z]+.)[0-9][0-9](?:0[1-9]|1[0-2])\\d{3}";
-        String patternTwo = ";\\d{0,19}=\\d{7}\\w*\\?";
+        String patternOne = "%B\\d{0,19}\\^[\\w\\s\\/]{2,26}\\^\\d{7}?";
+        String patternTwo = ";\\d{0,19}=\\d{14}?";
         
         Pattern trackOnePattern = Pattern.compile(patternOne);
         Pattern trackTwoPattern = Pattern.compile(patternTwo);
@@ -44,35 +44,33 @@ public class Track {
         		matchOne = trackOnePattern.matcher(s);
         		matchTwo = trackTwoPattern.matcher(s);
 	     		if (matchOne.find( )) {
-					i++;
+				i++;
 						
-		     			String str = matchOne.group(0);
-					str = str.replace('/','\0');
-		     			str = str.replace('^','\0');
-						
-					cnum = str.substring(0,16);
-		     			cname = str.substring(16,str.length()-8);
-					year = str.substring(str.length()-7,str.length()-5);
-		     			month = str.substring(str.length()-5,str.length()-3);
+		     		String str = matchOne.group(0);
+				str = str.replace('/','\0');
+	     			str = str.replace('^','\0');
+		   		str = str.replace('=','\0');
+
+				cnum = str.substring(2,19);
+		     		cname = str.substring(19,str.length()-8);
+				year = str.substring(str.length()-7,str.length()-5);
+	     			month = str.substring(str.length()-5,str.length()-3);
 					
-				     		
-					System.out.println("<Information of the "+ i +" credit card>");
-		     			System.out.println("Cardholder's Name: " +cname);
-					System.out.println("Card Number: " +cnum);
-	  	         		System.out.println("Expiration Date: " +month+ "/20" +year);
-					
+				System.out.println("<Information of the "+ i +" credit card>");
+				System.out.println("Cardholder's Name: " +cname);
+				System.out.println("Card Number: " +cnum);
+	  	       		System.out.println("Expiration Date: " +month+ "/20" +year);
 	     		}
-	     		if(matchTwo.find()){
-	     			
+	     		if(matchTwo.find()){	
 	     			String str = matchTwo.group(0);
-	     			pin = str.substring(str.length()-10,str.length()-6);
-	     			cvv = str.substring(str.length()-6,str.length()-3);
+	     			pin = str.substring(str.length()-7,str.length()-3);
+	     			cvv = str.substring(str.length()-3,str.length());
 	     			
 	     			System.out.println("Encrypted PIN: " +pin);
   	         		System.out.println("CVV Number: " +cvv);
 	     		}
-			}   
-        }
+		}   
+	}
 		
 	catch (IOException e) {
 		e.printStackTrace();
