eb088234c49deda5d1b4a63c88d8f070587e0297
Fenil Patel <fenil_patel93@ymail.com>
2017-01-31 17:46:07 -0700

    Changed Track 1 regular expression

--- src/Track.java
+++ src/Track.java
@@ -7,14 +7,14 @@ public class Track {
 
 	public static void main(String[] args) {
 		
-	String cname = null;
+		String cname = null;
         String cnum = null;
         String month = null;
         String year = null;
         String cvv = null;
         String pin = null;
-	String s = null;
-	BufferedReader buffer = null;
+        String s = null;
+        BufferedReader buffer = null;
         Matcher matchOne = null;
         Matcher matchTwo = null;
 
@@ -26,56 +26,56 @@ public class Track {
         
         int i = 0;
         
-        try {
-        	
+        try {	
         	String filePath = null;
         	
-        	if(args.length > 0)
-        	{
+        	if(args.length > 0){
         		filePath = args[0];
         	}
-        	else
-        	{
-        		filePath = "memorydump.dmp";
+        	
+        	else{
+        		filePath = "/Users/Fenil/Desktop/CSCI 476/Labs/Lab 1/memorydump.dmp";
         	}
         	
         	buffer = new BufferedReader(new FileReader(filePath));
         	while ((s = buffer.readLine()) != null) {
         		matchOne = trackOnePattern.matcher(s);
         		matchTwo = trackTwoPattern.matcher(s);
-	     		if (matchOne.find( )) {
-				i++;
+        		
+        		if (matchOne.find( )) {
+        			i++;
 						
-		     		String str = matchOne.group(0);
-				str = str.replace('/','\0');
-	     			str = str.replace('^','\0');
-		   		str = str.replace('=','\0');
+        			String str = matchOne.group(0);
+        			str = str.replace('/','\0');
+        			str = str.replace('^','\0');
+        			str = str.replace('=','\0');
 
-				cnum = str.substring(2,19);
-		     		cname = str.substring(19,str.length()-8);
-				year = str.substring(str.length()-7,str.length()-5);
-	     			month = str.substring(str.length()-5,str.length()-3);
+        			cnum = str.substring(2,19);
+        			cname = str.substring(19,str.length()-8);
+        			year = str.substring(str.length()-7,str.length()-5);
+        			month = str.substring(str.length()-5,str.length()-3);
 					
-				System.out.println("<Information of the "+ i +" credit card>");
-				System.out.println("Cardholder's Name: " +cname);
-				System.out.println("Card Number: " +cnum);
-	  	       		System.out.println("Expiration Date: " +month+ "/20" +year);
-	     		}
-	     		if(matchTwo.find()){	
-	     			String str = matchTwo.group(0);
+        			System.out.println("<Information of the "+ i +" credit card>");
+        			System.out.println("Cardholder's Name: " +cname);
+        			System.out.println("Card Number: " +cnum);
+        			System.out.println("Expiration Date: " +month+ "/20" +year);
+        		}
+	     		
+        		if(matchTwo.find()){	
+        			String str = matchTwo.group(0);
 	     			pin = str.substring(str.length()-7,str.length()-3);
 	     			cvv = str.substring(str.length()-3,str.length());
 	     			
 	     			System.out.println("Encrypted PIN: " +pin);
   	         		System.out.println("CVV Number: " +cvv);
 	     		}
-		}   
-	}
+        	}   
+        }
 		
-	catch (IOException e) {
-		e.printStackTrace();
-	} 
-		System.out.println("There is "+ i +" piece of credit card information in the memory data!");
+        catch (IOException e) {
+        	e.printStackTrace();
+        } 
+		
+        System.out.println("There is "+ i +" piece of credit card information in the memory data!");
 	}
-
 }
